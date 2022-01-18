72a906d73983174ecf5625ae0952bfdb561ef5f6
Kiara <kiarabroekhuizen1@hotmail.com>
2022-01-18 19:35:16 +0100

    pre processing finished :fire:

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 d3.tsv('data.txt').then(data => {
     console.log(typeof(data))
     var selection = data.slice(0, 20)
-      for (let i = 0; i < selection.length; i++){
-        console.log(selection[i].scientificName)
-        console.log(selection[i].eventDate)
-        console.log(selection[i].level0Name)
-    }
+    console.log(selection)
+    //   for (let i = 0; i < selection.length; i++){
+    //     console.log('Nieuw object start hier')
+    //     console.log(selection[i].scientificName)
+    //     console.log(selection[i].eventDate)
+    //     console.log(selection[i].level0Name)
+    //     console.log(selection[i].occurrenceRemarks)
+    // }
 
 
 })
--- preprocessing.js
+++ preprocessing.js
@@ -1,17 +1,24 @@
 const fs = require('fs')
 const d3 = require('d3')
 
+const settings = {
+    fileName: 'data',
+    filterData: true,
+    removeResidenceData: false
+}
+
 
 
 
 
 loadFile()
+
 function loadFile() {
 
-    fs.readFile("data.txt",{ encoding: 'utf8' }, function (err, data) {
+    fs.readFile("data.txt", { encoding: 'utf8' }, function (err, data) {
         if (!err) {
             //   const parsedData = d3.tsvParse(data);
-            console.log('received data items: ' +(data.length));
+            console.log('received data items: ' + (data.length));
             parseData(data)
         } else {
             console.log(err);
@@ -20,22 +27,44 @@ function loadFile() {
 }
 
 
-function parseData(source){
-  
-    const data = d3.tsvParse(source)
+function parseData(source) {
+
+    const data = d3.shuffle(d3.tsvParse(source))
     console.log("#Entries in data: ", data.length)
-    let selection = data.slice(0, 300)
-    console.log(selection)
+    let selection = settings.filterData ? data.map(filterProperties) : data.slice(0, 10)
+    console.log(selection.slice(0, 5))
+    writeDataFile(selection)
 }
 
 
-// fs.loadFile('data.txt')
-//     .pipe(tsv())
-//     .on('data', (data) => results.push(data))
-//     .on('end', () => {
-//         console.log(results);
-//         // [
-//         //   { NAME: 'Daffy Duck', AGE: '24' },
-//         //   { NAME: 'Bugs Bunny', AGE: '22' }
-//         // ]
-//     });
+// next step: need to write the new data into new file with fs
+function writeDataFile(data, fileIndex = 0) {
+    fs.writeFile(settings.fileName + "_" + fileIndex + ".json",
+        JSON.stringify(data, null, 4),
+        { encoding: 'utf8', flag: 'wx' },
+        function (err) {
+
+            if (err && err.code == "EEXIST") {
+                writeDataFile(data, ++fileIndex)
+            } else if (err) {
+                return console.log(err)
+            } else {
+                console.log("The file was saved!", (settings.fileName + "_" + fileIndex + ".json"))
+            }
+        })
+}
+
+function filterProperties(item) {
+    return {
+        id: item['gbifID'],
+        name: item['scientificName'],
+        kingdom: item['kingdom'],
+        class: item['class'],
+        order: item['order'],
+        phylum: item['phylum'],
+        family: item['family'],
+        date: item['eventDate'],
+        place: item['level0Name'],
+        description: item['occurrenceRemarks']
+    }
+}
